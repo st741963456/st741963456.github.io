1e4c9e6a7456226940a4b8a92129e107b1e8264b
st741963456 <36965820+st741963456@users.noreply.github.com>
2022-06-17 10:16:09 +0800

    Add files via upload

---
+++ App_Data/SQLQuery1.sql
@@ -0,0 +1,4 @@
+﻿--select * from [Member] where [address] like N'臺北____基隆%'
+--update [Member] set [age] = 16 where [id] = 'john'
+insert into [Member] ([id], [password], [sex], [age], [phone], [address]) values ('alice', '13579', 0, 37, '03-5712121', N'新竹市大學路1001號')
+--delete from [Member] where [id] = 'alice'
